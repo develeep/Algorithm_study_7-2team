1473e008c173b787f048d026ed5758719367c009
hiyeonseo032 <hiyeonseo032@naver.com>
2025-04-04 08:51:39 +0900

    [Solve]:덩치 문제 해결

---
+++ kimchaejun/BOJ/7568/sol.py
@@ -0,0 +1,18 @@
+N = int(input())
+weights, heights = [], []
+res = [1] * N
+for _ in range(N):
+    w, h = map(int, input().split())
+    weights.append(w)
+    heights.append(h)
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+        if weights[i] > weights[j] and heights[i] < heights[j]:
+            continue
+        if weights[i] < weights[j] and heights[i] > heights[j]:
+            continue
+        if weights[i] > weights[j] and heights[i] > heights[j]:
+            res[j] += 1
+print(*res)
